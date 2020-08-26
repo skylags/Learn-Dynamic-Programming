6fa7dc01fc794e9840be99466732459d78536ab6
Umang Agrawal <umang.18bcs1041@abes.ac.in>
2020-08-26 08:01:22 +0530

    Create CountofSubsetsWithGivenDifference.cpp

---
+++ CountofSubsetsWithGivenDifference.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+/*
+ What is Dynamic Programming.?
+ Basically, to recognize DP Problems, we have choices.
+ DP = Recursion + Some Memorization
+ In Recursive solution, we have a base condition and a choice diagram.
+ We memorize it to make DP.
+ */
+
+/*
+No of Subsets with given difference.
+Find no of subset pairs such that sum of both is S1 & S2.
+Find no of subsets pairs such that S1-S2 = dif, where dif will be given in the problem.
+Let's analyse a bit.
+Assume Sum as total sum of array elements.
+S1-S2 = dif ->(1)
+S1+S2 = Sum ->(2)
+Solving the above (1) & (2), we get,
+S1 = (Sum+dif)/2 - (3),
+Hence, this problem is reduced to finding the no of Subsets whose sum is S1 as calculated
+in (3).
+Refer to CountofSubsetsSum above for the approach to do tis . :)
+ */
+
+
+/*
+ Umang Agrawal
+ */
+
+
